0350c087ad770f93fffa73556646baa237187569
Ērika Tālberga <103637112+zanz1bra@users.noreply.github.com>
2023-11-13 18:05:22 +0200

    weather app home task first part
    labels are not where they should be. i have added constraints, but when launching, they jump to top leading corner

--- WeatherAPI/WeatherAPI/WeatherAPI/Controller/WeatherController.swift
+++ WeatherAPI/WeatherAPI/WeatherAPI/Controller/WeatherController.swift
@@ -9,13 +9,20 @@ import UIKit
 import Alamofire
 
 class WeatherViewContoller: UIViewController {
-
-    let apiKey:String = "" 
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var feelsLikeTempLabel: UILabel!
+    
+    
+    let apiKey:String = "5eb699a248msh0f0f3ad674a8a11p108c15jsnce53d5ab2ac9"
     let apiHost:String = "weatherapi-com.p.rapidapi.com"
     let apiUrl:String = "https://weatherapi-com.p.rapidapi.com/current.json"
     let city: String = "Riga"
     
-    var currentWeather:CurrentWeather?
+    var currentWeather: CurrentWeather?
+    var condition: Condition?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,22 +33,40 @@ class WeatherViewContoller: UIViewController {
         let headers:[String:String] = ["X-RapidAPI-Key": apiKey,
             "X-RapidAPI-Host": apiHost]
         let params:[String:String] = ["q":city]
+        
+        cityNameLabel.text = "Loading..."
+        conditionLabel.text = "Loading..."
+        tempLabel.text = "Loading..."
+        feelsLikeTempLabel.text = "Loading..."
+        
         AF.request(apiUrl,method: .get,parameters: params,headers: HTTPHeaders(headers)).responseDecodable(of:CurrentWeather.self){
             response in
             switch response.result{
             case .success(let value):
-                do{
-                    self.currentWeather = value
-                }
-                catch{
-                    print("error::::",error)
-                }
+                self.currentWeather = value
+                self.showData()
+//                do{
+//                    self.currentWeather = value
+//                }
+//                catch{
+//                    print("error::::",error)
+//                }
             case.failure(let error):
                 print(error)
+                self.cityNameLabel.text = "Error..."
+                self.conditionLabel.text = "Error..."
+                self.tempLabel.text = "Error..."
+                self.feelsLikeTempLabel.text = "Error..."
             }
         }
     }
     
+    func showData() {
+        cityNameLabel.text = currentWeather?.location.name
+        conditionLabel.text = condition?.text
+        tempLabel.text = "\(currentWeather?.current.tempC ?? 0.0) ºC"
+        feelsLikeTempLabel.text = "\(currentWeather?.current.feelslikeC ?? 0.0) ºC"
+    }
 
 
 }
