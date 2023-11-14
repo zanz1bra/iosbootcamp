b27a834c9ac1d6afe189903d2cd5a46b36c41e4e
Ērika Tālberga <103637112+zanz1bra@users.noreply.github.com>
2023-11-14 13:14:49 +0200

    Basic functionality, asking for location permissioin

--- WeatherAPI/WeatherAPI/WeatherAPI/Controller/WeatherController.swift
+++ WeatherAPI/WeatherAPI/WeatherAPI/Controller/WeatherController.swift
@@ -7,35 +7,53 @@
 
 import UIKit
 import Alamofire
+import CoreLocation
 
-class WeatherViewContoller: UIViewController {
+class WeatherViewContoller: UIViewController, CLLocationManagerDelegate {
     
-    @IBOutlet weak var cityNameLabel: UILabel!
-    @IBOutlet weak var conditionLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var feelsLikeTempLabel: UILabel!
-    
+    @IBOutlet weak var inputCity: UITextField!
     
     let apiKey:String = "5eb699a248msh0f0f3ad674a8a11p108c15jsnce53d5ab2ac9"
     let apiHost:String = "weatherapi-com.p.rapidapi.com"
     let apiUrl:String = "https://weatherapi-com.p.rapidapi.com/current.json"
-    let city: String = "Riga"
+    var location: String = ""
     
     var currentWeather: CurrentWeather?
-    var condition: Condition?
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadWeatherData()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        self.inputCity.text = location
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if(location.horizontalAccuracy > 0) {
+            self.locationManager.stopUpdatingLocation()
+            let long = String(location.coordinate.longitude)
+            let lat = String(location.coordinate.latitude)
+            self.location = lat + "," + long
+            self.loadWeatherData()
+        }
+    }
+    
+    @IBAction func getTemp(_ sender: Any) {
+        self.location = self.inputCity.text!
+    }
+    
     
     func loadWeatherData(){
         let headers:[String:String] = ["X-RapidAPI-Key": apiKey,
             "X-RapidAPI-Host": apiHost]
-        let params:[String:String] = ["q":city]
+        let params:[String:String] = ["q":self.location]
         
-        cityNameLabel.text = "Loading..."
-        conditionLabel.text = "Loading..."
         tempLabel.text = "Loading..."
         feelsLikeTempLabel.text = "Loading..."
         
@@ -43,31 +61,20 @@ class WeatherViewContoller: UIViewController {
             response in
             switch response.result{
             case .success(let value):
+                
                 self.currentWeather = value
-                self.showData()
-//                do{
-//                    self.currentWeather = value
-//                }
-//                catch{
-//                    print("error::::",error)
-//                }
+                self.tempLabel.text = (self.currentWeather?.current.tempC?.description ?? "...") + " ºC"
+                self.feelsLikeTempLabel.text = (self.currentWeather?.current.feelslikeC?.description ?? "...") + " ºC"
+                self.inputCity.text = (self.currentWeather?.location.name ?? self.inputCity.text)
+
             case.failure(let error):
                 print(error)
-                self.cityNameLabel.text = "Error..."
-                self.conditionLabel.text = "Error..."
                 self.tempLabel.text = "Error..."
                 self.feelsLikeTempLabel.text = "Error..."
             }
         }
     }
     
-    func showData() {
-        cityNameLabel.text = currentWeather?.location.name
-        conditionLabel.text = condition?.text
-        tempLabel.text = "\(currentWeather?.current.tempC ?? 0.0) ºC"
-        feelsLikeTempLabel.text = "\(currentWeather?.current.feelslikeC ?? 0.0) ºC"
-    }
-
 
 }
 
